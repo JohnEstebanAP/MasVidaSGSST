375a8199140eacb275f52a58a3132cf220a1a987
John Esteban Alvarez Piedrahita <esteban.ea145@gmail.com>
2021-11-29 22:24:18 -0500

    Implementacion del Login Con Google

--- app/src/main/java/com/johnestebanap/myapplication/LoginActivity.java
+++ app/src/main/java/com/johnestebanap/myapplication/LoginActivity.java
@@ -1,6 +1,7 @@
 package com.johnestebanap.myapplication;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.AlertDialog;
@@ -13,17 +14,26 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 public class LoginActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
     SharedPreferences preferences;
     SharedPreferences.Editor editor;
 
-    Button btnEntrar, btnRegistro;
+    private int GOOGLE_SING_IN = 100;
+    Button btnEntrar, btnRegistro, btnGoogle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,12 +41,55 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         mAuth = FirebaseAuth.getInstance();
-
         btnEntrar = findViewById(R.id.btn_entrar);
         btnRegistro = findViewById(R.id.registrarse);
+        btnGoogle = findViewById(R.id.btn_google);
 
         btnEntrar.setOnClickListener(view -> loginUser());
         btnRegistro.setOnClickListener(view -> registroUser());
+        btnGoogle.setOnClickListener(view -> loginUserGoogle());
+    }
+
+    public void loginUserGoogle() {
+        //Configuracion
+        GoogleSignInOptions googleConf = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestIdToken(getString(R.string.default_web_client_id)).requestEmail().build();
+        GoogleSignInClient googleClient = GoogleSignIn.getClient(this, googleConf);
+        googleClient.signOut();
+        startActivityForResult(googleClient.getSignInIntent(), GOOGLE_SING_IN);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == GOOGLE_SING_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                if(account != null){
+                    AuthCredential credential = GoogleAuthProvider.getCredential(account.getIdToken(), null);
+                    FirebaseAuth.getInstance().signInWithCredential(credential).addOnCompleteListener(
+                            new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()) {
+                                        Toast.makeText(LoginActivity.this, "Bienvenid@", Toast.LENGTH_SHORT).show();
+                                        showHome(account.getEmail());
+                                    } else {
+                                        showAlertError();
+                                        Log.w("TAG", "Error", task.getException());
+                                    }
+                                }
+                            }
+                    );
+                }
+            } catch (ApiException e) {
+                e.printStackTrace();
+                showAlertError();
+            }
+
+
+        }
     }
 
     public void loginUser() {
@@ -101,6 +154,7 @@ public class LoginActivity extends AppCompatActivity {
             }
         }
     }
+
     private void showHome(String email) {
         Intent intent = new Intent(this, HomeActivity.class);
         intent.putExtra("email", email);
@@ -115,7 +169,7 @@ public class LoginActivity extends AppCompatActivity {
         alertDialog.create().show();
     }
 
-    public void registroUser(){
+    public void registroUser() {
         startActivity(new Intent(this, RegistroActivity.class));
     }
 }
